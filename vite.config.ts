197f343f8d12a7453db138e5a6438cefe7959a56
laspinko <cat.radoslavov@gmail.com>
2025-04-27 14:41:43 +0300

    publishing configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: { outDir: "build" },
+  base: "/crochet-pattern-generator/",
   plugins: [react()],
-})
+});
